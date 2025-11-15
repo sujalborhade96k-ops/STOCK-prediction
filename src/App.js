1aa8938923927dadacc4f490d6c0b0f1302d759a
sujalborhade96k-ops <sujalborhade96k@gmail.com>
2025-11-15 11:14:35 +0530

    Create initial App component for Stock Prediction App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Stock Prediction App</h1>
+      <p>Deployment Successful!</p>
+      <p>The application is building. The full UI will appear shortly.</p>
+    </div>
+  );
+}
+
+export default App;
